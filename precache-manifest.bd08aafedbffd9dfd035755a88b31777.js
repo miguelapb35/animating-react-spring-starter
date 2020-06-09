7ced41feb75d89064d8f7233aead32f621ea9106
Miguel Angel <miguelapb35@gmail.com>
2020-06-09 11:36:41 -0500

    Updates

---
+++ precache-manifest.bd08aafedbffd9dfd035755a88b31777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba41daf843d3f1ea7f5c",
+    "url": "/animating-react-spring-starter/static/css/main.a2454576.chunk.css"
+  },
+  {
+    "revision": "ba41daf843d3f1ea7f5c",
+    "url": "/animating-react-spring-starter/static/js/main.18e81a85.chunk.js"
+  },
+  {
+    "revision": "9e1c26a22469d04a8be7",
+    "url": "/animating-react-spring-starter/static/js/runtime~main.d2d5e778.js"
+  },
+  {
+    "revision": "e181264dc5d14aaa0c72",
+    "url": "/animating-react-spring-starter/static/js/2.8c3bec86.chunk.js"
+  },
+  {
+    "revision": "c131d4c26c92b5bb55302172981ba6f3",
+    "url": "/animating-react-spring-starter/static/media/logo.c131d4c2.svg"
+  },
+  {
+    "revision": "c765054d1c41ba8eee8ee34ac4b876d2",
+    "url": "/animating-react-spring-starter/index.html"
+  }
+];
